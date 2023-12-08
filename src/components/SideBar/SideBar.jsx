aab101f34347f335ce1aa59cd18e04ded5dd1977
dhrutisubham <dhruti.subham@gmail.com>
2023-12-08 23:45:47 +0530

    added middle and bottom options without dropdown

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,11 +2,21 @@ import React from 'react'
 import './SideBar.css'
 
 
-const navPanel={
+const navPanelTop={
   'Home': 'home',
   'Questions': 'question',
   'Connections': 'connections',
   'Projects' : 'projects',
+}
+const navPanelMid={
+  'Clubs': ['home'],
+  'Societies': ['question'],
+}
+
+const navPanelBottom={
+  'Trumio Fans': 'fans',
+  'Trumio Playground': 'playground',
+  'Trumio MarketPlace': 'marketplace',
 }
 const isSelected={
   'Home': true,
@@ -25,17 +35,58 @@ const SideBar = () => {
       />
       </a>
 
-      <section className='flex flex-col py-5 drop-shadow bg-white1 z-0 relative'>
+
+      {/*Top Options */}
+      <section className='flex flex-col drop-shadow bg-white1 z-0 relative pt-2'>
       {
-        Object.entries(navPanel).map(([label, icon])=>(
-          <a className='flex gap-5 hover:bg-white2 pl-7 p-3' href="./">
+        Object.entries(navPanelTop).map(([label, icon])=>(
+          <a className='option' href="./">
             <img
               className='navPanelOptionLogo'
               src={`public/assets/icons/SideBar/Navpanel/${icon}${isSelected[label]?'-selected':''}.svg`}
               alt={`${label} Icon`}
             ></img>
             <span 
-            className={isSelected[label]?'font-bold text-black1':'font-regular text-grey1'}>
+            className={`font-${isSelected[label]?'bold text-black1':'regular text-grey1'} text-sm`}>
+              {label}</span>
+          </a>
+        ))
+      }
+      </section>
+
+
+      {/* Middle Dropdown Options */}
+
+      <section className='flex flex-col drop-shadow bg-white1 -z-[8] relative py-2'>
+      {
+        Object.entries(navPanelMid).map(([label, array])=>(
+          <a className='option' href="./">
+            <img
+              className='navPanelOptionLogo -rotate-90'
+              src={`public/assets/icons/SideBar/Navpanel/dropdown.svg`}
+              alt={`Dropdown Icon`}
+            ></img>
+            <span 
+            className='font-regular text-black1 text-sm'>
+              {label}</span>
+          </a>
+        ))
+      }
+      </section>
+      
+
+      {/* Bottom Options */}
+      <section className='flex flex-col drop-shadow bg-white1 -z-10 relative pt-2'>
+      {
+        Object.entries(navPanelBottom).map(([label, icon])=>(
+          <a className='option' href="./">
+            <img
+              className='navPanelOptionLogo'
+              src={`public/assets/icons/SideBar/Navpanel/${icon}.svg`}
+              alt={`${label} Icon`}
+            ></img>
+            <span 
+            className='font-regular text-blue1 text-sm'>
               {label}</span>
           </a>
         ))
